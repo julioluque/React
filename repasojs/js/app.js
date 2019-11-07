abae51983a39faea9a9e6985c82a120239770b3d
julio luque <jluque@tecnosoftware.com>
2019-11-07 17:13:48 -0300

    29. Módulos en ES6 Exportar Importar

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,21 +1,3 @@
-// escribiendo clases.
+import descripcionTarea  from './tareas.js';
 
-class Tarea {
-    constructor (nombre, prioridad){
-        this.nombre = nombre;
-        this.prioridad = prioridad;
-    }
-    mostrar (){
-        return `${this.nombre} tiene una prioridad ${this.prioridad}`;
-    }
-}
-
-let tarea1 = new Tarea('Armar CV', 'Alta');
-let tarea2 = new Tarea('Preparar cafe', 'Alta');
-let tarea3 = new Tarea('Pasear al perro', 'media');
-let tarea4 = new Tarea('Conocer a mis suegros', 'baja');
-
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-console.log(tarea4.mostrar());
+console.log (descripcionTarea);
